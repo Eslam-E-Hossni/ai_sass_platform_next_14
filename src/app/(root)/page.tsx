d0d3aec2f636af9bd70e01c7fd7e457ab20e2616
Islam <islam@sonono.ch>
2024-03-21 16:59:10 +0200

    fix conflict

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 const Home = () => {
-<<<<<<< Updated upstream:src/app/page.tsx
-  return (
-    <div>
-      Home
-    </div>
-  );
-=======
   return <div>Home</div>;
->>>>>>> Stashed changes:src/app/(root)/page.tsx
 };
 
 export default Home;
