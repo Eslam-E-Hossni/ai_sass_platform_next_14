55768a7001790e616f47580b76802ee8c6232a19
Islam <islam@sonono.ch>
2024-03-21 16:53:07 +0200

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Home = () => {
+<<<<<<< Updated upstream:src/app/page.tsx
   return (
     <div>
     </div>
   );
+=======
+  return <div>Home</div>;
+>>>>>>> Stashed changes:src/app/(root)/page.tsx
 };
 
 export default Home;
